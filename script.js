e761761247ceebc9dff52c484516e10d8a631bff
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-08-30 20:19:34 +0530

    calculating sum

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ const luckyNumber = document.querySelector("#lucky-number");
 const checkNumber = document.querySelector("#check-number");
 
 
-checkNumber.addEventListener("click", function getValues(){
-    console.log(dateOfBirth.value, luckyNumber.value);
-});
+function calaculateSum(dob){
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for(i = 0; i < dob.length; i++){
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
+
+function checkBirthDateIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calaculateSum(dob);
+    console.log(sum);
+}
+
+checkNumber.addEventListener("click", checkBirthDateIsLucky);
